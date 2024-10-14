5dbb6a5d184f6da0b1c699e96f96d39183e50627
Kamal Thakur <kamal.thakur.dev16@gmail.com>
2024-10-14 22:04:42 +0530

    Added Tabs to Save SendersList and Save Error Log Object, added Custom Metadata Types

--- force-app/main/default/lwc/errorBuffAI/errorBuffAI.js
+++ force-app/main/default/lwc/errorBuffAI/errorBuffAI.js
@@ -2,11 +2,14 @@ import { LightningElement, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import updateErrorLogMetadata from '@salesforce/apex/ErrorLogMetadataUpdater.updateErrorLogMetadata';
 import getCustomObjectApiNames from '@salesforce/apex/ErrorLogMetadataUpdater.getCustomObjectApiNames';
+import updateSenderList from '@salesforce/apex/ErrorLogMetadataUpdater.updateSenderList';
 
 export default class ErrorLogMetadataCreator extends LightningElement {
     @track objectApiNames = [];
     @track selectedObjectApiName = '';
+    @track emailList = '';
 
+    // Fetch available custom object API names
     @wire(getCustomObjectApiNames)
     wiredCustomObjectApiNames({ data, error }) {
         if (data) {
@@ -31,6 +34,24 @@ export default class ErrorLogMetadataCreator extends LightningElement {
                 });
         } else {
             this.showToast('Error', 'Please select an object before creating metadata', 'error');
+        }
+    }
+
+    handleEmailListChange(event) {
+        this.emailList = event.target.value;
+    }
+
+    handleUpdateSenderList() {
+        if (this.emailList) {
+            updateSenderList({ emailList: this.emailList })
+                .then(() => {
+                    this.showToast('Success', 'Sender list updated successfully', 'success');
+                })
+                .catch((error) => {
+                    this.showToast('Error', 'Error updating sender list: ' + error.body.message, 'error');
+                });
+        } else {
+            this.showToast('Error', 'Please enter email addresses before updating sender list', 'error');
         }
     }
 
